e64d8f878f2c5f72f4f373f591b031757fdf0180
mateusmaciel340 <mateusmaciel340@gmail.com>
2023-05-26 17:03:34 -0300

    refactor: interação do usuário

--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,19 @@
-import { handleStatus, log } from "./utils/promise-helpers.js";
 import './utils/array-helpers.js';
 import { notasService as service } from "./nota/service.js";
+import { takeUntil, debounceTime, partialize, pipe } from "./utils/operators.js";
+
+const operations = pipe(
+    partialize(takeUntil, 3),
+    partialize(debounceTime, 500)
+);
+
+const action = operations(() => 
+    service
+    .sumItens('2143')
+    .then(console.log)
+    .catch(console.log)
+);
 
 document
     .querySelector('#myButton')
-    .onclick = () =>
-        service
-            .sumItens('2143')
-            .then(console.log)
-            .catch(console.log);
+    .onclick = action;
