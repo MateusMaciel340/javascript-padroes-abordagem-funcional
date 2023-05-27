e6b446a9bd8b7ec7bf66acdd79dce2c12ca4b93a
mateusmaciel340 <mateusmaciel340@gmail.com>
2023-05-27 14:29:59 -0300

    feat: pattern publisher e subscriber

--- public/app/app.js
+++ public/app/app.js
@@ -1,7 +1,8 @@
 import { timeoutPromise, retry } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
-import { notasService as service } from "./nota/service.js";
-import { takeUntil, debounceTime, partialize, pipe } from "./utils/operators.js";
+import { notasService as service } from './nota/service.js';
+import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
+import { EventEmitter } from './utils/event-emitter.js';
 
 const operations = pipe(
     partialize(takeUntil, 3),
@@ -10,7 +11,7 @@ const operations = pipe(
 
 const action = operations(() => 
     retry(3, 3000, () => timeoutPromise(200, service.sumItens('2143')))
-    .then(console.log)
+    .then(total => EventEmitter.emit('itensTotalizados', total))
     .catch(console.log)
 );
 
